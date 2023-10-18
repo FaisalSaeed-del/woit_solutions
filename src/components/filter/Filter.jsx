200c53f5ede9939b43623ea80490cfacb1098c42
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2023-10-18 20:08:13 +0500

    done

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Filter = () => {
   return (
-    <div className="bg-[#1D1627]">
-      <div className="container mt-10">
-        <div className="flex  justify-betweenborder-[1px] border-borderColor w-full rounded-[20px]">
+    <div className="bg-[#1D1627] border-[1px] border-borderColor w-full rounded-[20px] mt-4  ">
+      <div className="container">
+        <div className="flex  justify-between">
           <div>
             <button className="flex items-center text-white text-[15px] border-borderColor border-[1px] rounded-[20px] py-2 px-3 gap-2 cursor-pointer">
               <svg
@@ -46,6 +46,7 @@ const Filter = () => {
               className="outline-none caret-borderColor bg-transparent"
             />
           </div>
+          <div className="flex gap-4">
           <div>
             <button className="flex border-borderColor border-[1px] items-center rounded-[20px] py-2 px-2 text-white text-[15px] gap-2">
               <svg
@@ -72,6 +73,27 @@ const Filter = () => {
               </svg>
               visible
             </button>
+          </div>
+          <div>
+            <button className="flex items-center gap-1 text-white text-[15px] font-medium border-borderColor border-[1px] py-2 px-2 rounded-[20px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+              >
+                <path
+                  d="M9.56545 7.33334L10.3737 5.66666M10.3737 5.66666L11.667 3L12.9558 5.66666M10.3737 5.66666H12.9558M12.9558 5.66666L13.7614 7.33334M9.83369 9.66664H13.5003L9.83369 14H13.5003M4.66699 3V14M4.66699 14L2.66699 12M4.66699 14L6.66699 12"
+                  stroke="white"
+                  stroke-width="1.2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              Sortby
+            </button>
+          </div>
           </div>
         </div>
       </div>
